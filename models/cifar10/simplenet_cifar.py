9952da06884275903365a6d32e6a8faa7ad560d4
Jordan Dotzel <jad443@cornell.edu>
2019-10-21 19:15:28 -0400

    Make simplenet slightly more complicated for testing

--- models/cifar10/simplenet_cifar.py
+++ models/cifar10/simplenet_cifar.py
@@ -22,17 +22,22 @@ __all__ = ['simplenet_cifar']
 class Simplenet(nn.Module):
     def __init__(self):
         super(Simplenet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5, padding=2)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.conv2 = nn.Conv2d(6, 16, 5, padding=2)
+        self.conv3 = nn.Conv2d(16, 32, 5, padding=2)
+        self.fc1 = nn.Linear(32 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+        x = x.view(-1, 32 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         #x = nn.Threshold(0.2, 0.0)#ActivationZeroThreshold(x)
